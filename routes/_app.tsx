c204b5a03868d9a86fed835a2e4f0401687d659b
keewinek <keewinek@gmail.com>
2025-07-09 23:12:41 +0200

    title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Vibe Coder</title>
+        <title>Vibe Coder - Let AI do your roblox scripting.</title>
         <link rel="stylesheet" href="/styles.css" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
