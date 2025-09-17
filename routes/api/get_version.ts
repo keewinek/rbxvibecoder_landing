5cc4be5cd9c4da60d04392175129df8f1a582e09
keewinek <keewinek@gmail.com>
2025-09-17 21:14:12 +0200

    new version updated in api

--- routes/api/get_version.ts
+++ routes/api/get_version.ts
@@ -1,6 +1,6 @@
 import { FreshContext } from "$fresh/server.ts";
 
-const CURRENT_VERSION = "Vibe Coder BETA v0.3";
+const CURRENT_VERSION = "Vibe Coder BETA v0.4";
 
 export const handler = (_req: Request, _ctx: FreshContext): Response => {
   return new Response(JSON.stringify({ version: CURRENT_VERSION }), {
