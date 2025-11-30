915a0f384df59e0f41160ae3651e219c25a796c3
Wojtek Kołacz <keewinek@MacBook-Pro-Wojtek.local>
2025-11-30 15:07:07 +0100

    0.5

--- routes/api/get_version.ts
+++ routes/api/get_version.ts
@@ -1,6 +1,6 @@
 import { FreshContext } from "$fresh/server.ts";
 
-const CURRENT_VERSION = "Vibe Coder BETA v0.4";
+const CURRENT_VERSION = "Vibe Coder BETA v0.5";
 
 export const handler = (_req: Request, _ctx: FreshContext): Response => {
   return new Response(JSON.stringify({ version: CURRENT_VERSION }), {
